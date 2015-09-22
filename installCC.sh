48212088e717db827456266af436c8b1472cd598
learnin9 <learnin9@gmail.com>
2015-09-22 18:10:58 +0800

    Update installCC.sh

--- installCC.sh
+++ installCC.sh
@@ -665,7 +665,8 @@ function UI() {
 function AST13PATCH() {
 	cd /usr/src
 	rm -rf /usr/lib/asterisk/modules/codec_g729-ast18-gcc4-glibc-x86_64-pentium4.so
-	wget http://asterisk.hosting.lv/bin/codec_g729-ast130-gcc4-glibc-x86_64-pentium4.so
+	wget http://asterisk.hosting.lv/bin/codec_g729-ast130-gcc4-glibc2.2-x86_64-pentium4.so
+	wget http://asterisk.hosting.lv/bin/codec_g723-ast130-gcc4-glibc2.2-x86_64-pentium4.so
 	cp /usr/src/codec_g729-ast130-gcc4-glibc-x86_64-pentium4.so /usr/lib/asterisk/modules/
 	cd /etc/asterisk
 	cat > /etc/asterisk/modules.conf << EOF
